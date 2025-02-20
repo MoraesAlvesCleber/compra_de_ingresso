a5d94311f4442a2e02448fee953a329e6c74c818
Cleber Moraes <alvesmoraesb@gmail.com>
2025-02-20 13:29:26 -0300

    inserindo condicional para impedir número negativo

--- js/app.js
+++ js/app.js
@@ -17,21 +17,22 @@ function comprar() {
     let avisoQuantidade = document.querySelector('.aviso1');
 
     let avisoExistente = document.querySelector(".aviso");
+
     if (avisoExistente) {
         avisoExistente.remove();
     }
 
 
-    if (produto === 'pista' && pista > 0 && qtd <= pista) {
+    if (produto === 'pista' && pista > 0 && qtd <= pista && qtd > 0) {
         pista -= qtd;
         valorPista.innerHTML = pista;
 
-    } else if (produto === 'superior' && superior > 0 && qtd <= superior) {
+    } else if (produto === 'superior' && superior > 0 && qtd <= superior && qtd > 0) {
         superior -= qtd;
         valorSuperior.innerHTML = superior;
 
 
-    } else if (produto === 'inferior' && inferior > 0 && qtd <= inferior) {
+    } else if (produto === 'inferior' && inferior > 0 && qtd <= inferior && qtd > 0) {
         inferior -= qtd;
         valorInferior.innerHTML = inferior;
 
